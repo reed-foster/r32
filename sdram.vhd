bd68c108ed079dbf2368ebe5a861d2a3d1c4a7c7
reed-foster <reed.a.foster@gmail.com>
2017-04-16 19:35:16 -0700

    working on finite state machine for sdram controller

--- sdram.vhd
+++ sdram.vhd
@@ -28,24 +28,26 @@ entity sdram is
     (
         --Processor Interface
         clock       : in  std_logic;
-        load        : in  std_logic;
-        cs          : in  std_logic;
+        read_req    : in  std_logic;
+        write_req   : in  std_logic;
         d_in        : in  std_logic_vector (15 downto 0);
         d_out       : out std_logic_vector (15 downto 0);
-        d_out_valid : out std_logic;
         addr        : in  std_logic_vector (23 downto 0);
+        read_ready  : out std_logic;
+        write_ready : out std_logic;
 
 
         --SDRAM Interface
         --clock
-        sdram_clk   : out std_logic; --synchronous clock input to SDRAM
-        cke         : out std_logic; --clock enable (HIGH) disable (LOW)
+        sdram_clk : out std_logic; --synchronous clock input to SDRAM
+        cke       : out std_logic; --clock enable (HIGH) disable (LOW)
 
         --data I/O mask: controls output buffers in read mode and masks input data in write mode
-        dqm : out std_logic_vector(1 downto 0); --byte mask
+        udqm : out std_logic; --byte mask
+        ldqm : out std_logic;
 
         --enable/disable signals
-        --control signals: ba0, ba1, cs, ras#, cas#, and we#
+        --control signals: ba0, ba1, cs#, ras#, cas#, and we#
         ba       : out std_logic_vector (1 downto 0); --bank activate "00" => A, "01" => B, etc.
         sdram_cs : out std_logic; --enables/disables command decoder
         ras      : out std_logic; --row address strobe
@@ -61,7 +63,10 @@ end sdram;
 
 architecture behavioral of sdram is
    
-   --type fsm_states is (idle, refresh, rowactivate, read, write);
+   type fsm_states is (init, idle, rd, wrt, ref);
+
+   signal currentstate : fsm_states := init;
+   signal nextstate : fsm_states;
 
    --commands in the form cs#, ras#, cas#, we#
     constant cmd_nop     : std_logic_vector(3 downto 0) := "0111";
@@ -69,11 +74,44 @@ architecture behavioral of sdram is
     constant cmd_write   : std_logic_vector(3 downto 0) := "0100";
     constant cmd_bnkact  : std_logic_vector(3 downto 0) := "0011";
     constant cmd_prechrg : std_logic_vector(3 downto 0) := "0010"; --A10 must be high
-    constant cmd_ref     : std_logic_vector(3 downto 0) := "0001";
+    constant cmd_refresh : std_logic_vector(3 downto 0) := "0001";
     constant cmd_setmode : std_logic_vector(3 downto 0) := "0000";
+    constant cmd_hltbrst : std_logic_vector(3 downto 0) := "0110";
+
+    signal command : std_logic_vector(20 downto 0);
+    signal dqm     : std_logic_vector(1 downto 0);
+    signal bank    : std_logic_vector(1 downto 0);
+    signal addr    : std_logic_vector(12 downto 0);
+    signal cmdlet  : std_logic_vector(3 downto 0);
 
 begin
 
+    sync_proc : process(clock, nextstate, ld, str)
+    begin
+        case currentstate is
+            when init =>
+
+            when idle =>
+
+            when rd =>
+
+            when wrt =>
+
+            when ref =>
+
+        end case;
+    end process sync_proc;
+
+    command <= dqm & bank & addr & cmd;
+
+    udqm <= command(20);
+    ldqm <= command(19);
+    ba <= command(18 downto 17);
+    ram_addr <= command(16 downto 4);
+    sdram_cs <= command(3);
+    ras <= command(2);
+    cas <= command(1);
+    we <= command(0);
 
 end behavioral;
 
