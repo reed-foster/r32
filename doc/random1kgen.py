d054f654e8abad806089fd11064e51b5e7548d68
reed-foster <reed.a.foster@gmail.com>
2017-06-26 19:12:16 -0700

    added file for generating 1kB rom blocks in vhdl

---
+++ doc/random1kgen.py
@@ -0,0 +1,15 @@
+import random
+import math
+
+randarray = [random.randint(0, 2 ** 16 - 1) for i in range(512)]
+formattedstr = ""
+
+for i in range(len(randarray)):
+    #array <= (0 => x"abcd", 1 => x"1234", ...)
+    hexval = hex(randarray[i])[2:]
+    formattedstr += str(i) + ' => x"' + hexval + '", '
+        #(" " * (2 - int(math.log(i + 0.5, 10)))) +
+        #("0" * (4 - len(hexval))) +
+    #if (i % 16 == 15 and i > 0):
+        #formattedstr += "\n"
+print formattedstr
