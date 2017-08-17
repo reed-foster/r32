38feee2c72e9e9981e57248093b7df59cc335190
reed_foster <reed.a.foster@gmail.com>
2017-08-17 09:35:26 -0700

    reorganized file structure

--- assembler/asmlib.py
+++ assembler/asmlib.py
@@ -1,14 +1,14 @@
 import re
 
 opcodes = ["add", "addi", "addiu", "addu", "and", "andi", "bc1f", "bc1t", "beq",
-           "bgez", "bgtz", "blez", "bltz", "bne", "ceq", "cfc1", "cge", "cgt",
-           "cle", "clt", "cne", "ctc1", "fabs", "fadd", "fdiv", "fma", "fmul",
-           "fneg", "fsqrt", "fsub", "j", "jr", "lb", "lbu", "lh", "lhu", "lui",
-           "lw", "lwc0", "lwc1", "mfc0", "mfc1", "mov", "mtc0", "mtc1", "nor",
-           "or", "ori", "pre", "sh", "sll", "sllv", "slt", "slti", "sltiu", "sltu",
-           "sra", "srav", "srl", "srlv", "sub", "sub", "subu", "sw", "swc0", "swc1",
-           "syscall", "teq", "teqi", "tge", "tgei", "tgeiu", "tgeu", "tlt", "tlti",
-           "tltiu", "tltu", "tne", "tnei", "xor", "xor"]
+           "bgez", "bgtz", "blez", "bltz", "bne", "ceq", "cfc1", "cle", "clt",
+           "ctc1", "fabs", "fadd", "fdiv", "fma", "fmul", "fneg", "fsqrt", "fsub",
+           "j", "jr", "lb", "lbu", "lh", "lhu", "lui", "lw", "lwc1", "mfc0",
+           "mfc1", "mov", "mtc0", "mtc1", "nor", "or", "ori", "pref", "sb", "sh",
+           "sll", "sllv", "slt", "slti", "sltiu", "sltu", "sra", "srav", "srl",
+           "srlv", "sub", "subu", "sw", "swc1", "syscall", "teq", "teqi", "tge",
+           "tgei", "tgeiu", "tgeu", "tlt", "tlti", "tltiu", "tltu", "tne", "tnei",
+           "xor", "xori"]
 
 directives = [".text", ".data", ".word", ".half", ".byte", ".space", ".fill", ".ascii", ".asciiz", ".float", ".globl"]
 
